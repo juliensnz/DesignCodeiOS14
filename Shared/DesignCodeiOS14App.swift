8d5cb077c337cab7823ec54e0c969df82eba9c25
Julien Sanchez <sumbobyboys@gmail.com>
2021-01-16 21:23:53 +0100

    Initial Commit

---
+++ Shared/DesignCodeiOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS14App.swift
+//  Shared
+//
+//  Created by Julien Sanchez on 16/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
